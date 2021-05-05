65fe06ec291c8c760d1a818191cce2241d921831
Ruder1 <cpc15@mail.ru>
2021-05-05 21:42:13 +0700

    Создана библеотека классов В ней созданы классы: Контакт, ПРоект, Менеджер проекта, Номер телефона

---
+++ ContactsAppUI/ContactsApp1/PhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp1
+{
+    public class PhoneNumber
+    {
+        private int _phoneNumber;
+
+        public string NumberPhone
+        {
+            get { return _phoneNumber; }
+            set
+            {
+
+            }
+        }
+    }
+}
